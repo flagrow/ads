1b8a401ad426603f5c59c94a4f444b381a25f21d
dshovchko <d.shovchko@gmail.com>
2017-05-16 17:59:46 +0300

    add a parameter for specifying the position from which to start the ads

--- js/forum/src/addAdBetweenPosts.js
+++ js/forum/src/addAdBetweenPosts.js
@@ -7,13 +7,14 @@ export default function() {
         const advertisement = app.forum.attribute('flagrow.ads.between-posts');
 
         if (advertisement && component.children.length) {
+            const start = parseInt(app.forum.attribute('flagrow.ads.start-from-post') || 1);
             const between = parseInt(app.forum.attribute('flagrow.ads.between-n-posts') || 5);
             // We need to copy all comments first, otherwise there is no way to detect and jump the last comment
             const commentPosts = component.children.filter(post => post.attrs['data-type'] == 'comment');
 
             // Insert an inside every n comment
             commentPosts.forEach((post, i) => {
-                if (i > 0 && (i + 1) % between === 0 && i < commentPosts.length - 1) {
+                if (i >= start && (i - start) % between === 0 && i < commentPosts.length - 1) {
                     post.children.push(
                         m('div.Flagrow-Ads-fake-poststream-item',
                             m('article.Post.EventPost',
