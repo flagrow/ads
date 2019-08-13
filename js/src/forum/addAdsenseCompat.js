44f401a207ef2ca39684367a4339a7b6ea7b9bb2
Charlie K <charlie.kern01@gmail.com>
2019-08-12 19:34:01 -0700

    Hopefully fix all adsense issues

--- js/src/forum/addAdsenseCompat.js
+++ js/src/forum/addAdsenseCompat.js
@@ -1,22 +1,26 @@
-import { extend } from 'flarum/extend';
+import {extend} from 'flarum/extend';
 import app from 'flarum/app';
 import Page from 'flarum/components/Page';
 import PostStream from 'flarum/components/PostStream';
 import IndexPage from 'flarum/components/IndexPage';
 
-export default function() {
-    extend(Page.prototype, 'config', function(component) {
-        if ($(".adsbygoogle").length) {
+export default function () {
+    extend(Page.prototype, 'config', function () {
+        if ($(".adsbygoogle").length && window.adsbygoogle) {
 
             if (app.header_ad_active !== true) {
-                (adsbygoogle = window.adsbygoogle || []).push({});
+                try {
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                } catch {
+                    return;
+                }
                 app.header_ad_active = true;
             }
 
             if (!app.ads_index) {
                 app.ads_index = app.history.stack.length
             } else if (app.ads_index !== app.history.stack.length) {
-                app.ads_index = app.history.stack.length
+                app.ads_index = app.history.stack.length;
                 $(".adsbygoogle").each(function (i, e) {
                     $(e).html($(e).html())
                 });
@@ -24,22 +28,25 @@ export default function() {
         }
     });
 
-    extend(PostStream.prototype, 'config', function() {
-        if ($(".adsbygoogle").length) {
-
-            if (app.cache[this.discussion.id() + 'ads_active'] !== true && app.previous !== undefined && window.adsbygoogle.loaded !== true) {
+    extend(PostStream.prototype, 'config', function () {
+        if ($(".adsbygoogle").length && window.adsbygoogle) {
+            try {
                 (adsbygoogle = window.adsbygoogle || []).push({});
-                app.cache[this.discussion.id() + 'ads_active'] = true;
+            } catch {
+                return;
             }
-
         }
     });
 
-    extend(IndexPage.prototype, 'config', function() {
-        if ($(".adsbygoogle").length) {
+    extend(IndexPage.prototype, 'config', function () {
+        if ($(".adsbygoogle").length && window.adsbygoogle) {
 
             if (app.index_ad_active !== true && window.adsbygoogle.loaded !== true) {
-                (adsbygoogle = window.adsbygoogle || []).push({});
+                try {
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                } catch {
+                    return;
+                }
                 app.index_ad_active = true;
             }
 
